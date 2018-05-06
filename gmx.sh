0ce3cbed610ea078e23bbf02042fe3f508b3395e
Olegzandr VD <alex@oresoftware.com>
2018-05-06 15:43:39 -0700

    pdev:'empty commit message'

--- gmx.sh
+++ gmx.sh
@@ -3,24 +3,15 @@
 
 gmx(){
 
-  local first_arg="$1";
-
-  if [[ ! -z "$first_arg" && "$first_arg" != "-"* ]]; then
-
     if [[ -d "node_modules" ]]; then
+
       PATH="./node_modules/.bin:$PATH" "$@"
+
     else
+
      local nm="$(gmx_find_root)"
      PATH="$nm:$PATH" "$@"
+
     fi
 
-  else
-
-   if [[ -z "$(which gmx)" ]]; then
-      ( npm install -g gmx ) &> /dev/null
-   fi
-
-   command gmx "$@"
-
-  fi
 }
