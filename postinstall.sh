5c8b7983412dd91fd8f5495d6bad75975753a3fb
Olegzandr VD <alex@oresoftware.com>
2018-05-08 23:08:16 -0700

    pdev:'empty commit message'

--- postinstall.sh
+++ postinstall.sh
@@ -17,11 +17,15 @@ cat dist/find-root.js > "$HOME/.gmx/find-root.js"
 
 root_gmx="/usr/local/bin/gmx"
 rm -rf "$root_gmx" || echo "could not remove '$root_gmx'";
-cat run.gmx.sh > "$root_gmx"
+cat run.gmx.sh > "$root_gmx" && chmod u+x "$root_gmx" || {
+  echo "Could not write file or change permissions for file '$root_gmx'"
+}
 
 gmx_bin="$(npm bin -g)/gmx";
 rm -rf "$gmx_bin" || echo "could not remove '$gmx_bin'";
-cat run.gmx.sh > "$gmx_bin"
+cat run.gmx.sh > "$gmx_bin" && chmod u+x "$gmx_bin" || {
+  echo "Could not write file or change permissions for file '$gmx_bin'"
+}
 
 echo -e "${gmx_green}GMX was installed successfully.${gmx_no_color}";
 echo -e "Add the following line to your .bashrc/.bash_profile files:";
