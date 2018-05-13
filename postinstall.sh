a71c1ac4832c26bf2a4fe9c6762aa0ff10a1db20
Olegzandr VD <alex@oresoftware.com>
2018-05-13 13:52:40 -0700

    pdev:'empty commit message'

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,11 @@
 
 set -e;
 
+if [[ "$gmx_skip_postinstall" == "yes" ]]; then
+   echo "GMX is skipping postinstall routine.";
+   exit 0;
+fi
+
 gmx_gray='\033[1;30m'
 gmx_magenta='\033[1;35m'
 gmx_cyan='\033[1;36m'
@@ -15,17 +20,20 @@ mkdir -p "$HOME/.gmx"
 cat gmx.sh > "$HOME/.gmx/gmx.sh"
 cat dist/find-root.js > "$HOME/.gmx/find-root.js"
 
-root_gmx="/usr/local/bin/gmx"
-rm -rf "$root_gmx" || echo "could not remove '$root_gmx'";
-cat run.gmx.sh > "$root_gmx" && chmod u+x "$root_gmx" || {
-  echo "Could not write file or change permissions for file '$root_gmx'"
-}
+(
 
-gmx_bin="$(npm bin -g)/gmx";
-rm -rf "$gmx_bin" || echo "could not remove '$gmx_bin'";
-cat run.gmx.sh > "$gmx_bin" && chmod u+x "$gmx_bin" || {
-  echo "Could not write file or change permissions for file '$gmx_bin'"
-}
+  cd "$HOME/.oresoftware/nodejs";
+  gmx_skip_postinstall=yes npm install gmx@latest
+) &
+
+
+if [[ -z "$(which gmx)" ]]; then
+    echo "installing GMX globally...."
+    gmx_skip_postinstall=yes npm install -g gmx
+fi
+
+wait;
+
 
 echo -e "${gmx_green}GMX was installed successfully.${gmx_no_color}";
 echo -e "Add the following line to your .bashrc/.bash_profile files:";
